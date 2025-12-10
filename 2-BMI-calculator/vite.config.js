94124f9c660ca9737ce1d6813a229f2402562d6c
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-10 13:09:54 +0530

    Updated docs folder

--- 2-BMI-calculator/vite.config.js
+++ 2-BMI-calculator/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-practice-projects/1-counter/',
+  base: '/react-practice-projects/2-BMI-calculator/',
 })
