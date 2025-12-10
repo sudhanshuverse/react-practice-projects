b6c51b5cc57ce2bfbcbf19ca5881342aed606a6f
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-10 11:01:42 +0530

    Added repo 1-counter

---
+++ 1-counter/src/App.jsx
@@ -0,0 +1,28 @@
+import './style.css';
+
+import reloadImg from '../public/assets/reload.svg'
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return(
+      <div className='parent-container'>
+        <div className='card-container'>
+          <div className='first-row'>
+            <img src={reloadImg} alt="reload image" onClick={() => setCount(0)}/>
+          </div>
+
+          <div className='second-row'>
+            <h1>{count}</h1>
+          </div>
+          
+          <div className='third-row'>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count>0 ? count - 1: 0)}>-</button>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default App;
