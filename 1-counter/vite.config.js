9ce2bfc5c5c81669c1e5737cb169ad6cbc8ba199
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-10 11:23:15 +0530

    Live code folders added

--- 1-counter/vite.config.js
+++ 1-counter/vite.config.js
@@ -1,7 +1,8 @@
+// 1-counter/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-practice-projects/1-counter/',
 })
