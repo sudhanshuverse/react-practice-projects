2258a3fe5ecf17e8a442c53beb39ed5b91648584
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-12 12:03:23 +0530

    Updated 5-character-and-sentence-counter folder

---
+++ 5-character-and-sentence-counter/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/rect-practice-projects/5-character-and-sentence-counter',
+})
