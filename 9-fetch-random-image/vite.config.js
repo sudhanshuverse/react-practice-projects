a8aac7faaad3caf4093337ca5a3dc22d6c5fe90e
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-13 19:10:29 +0530

    vite.config.js file

--- 9-fetch-random-image/vite.config.js
+++ 9-fetch-random-image/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-practice-projects/9-fetch-random-image',
+  base: '/react-practice-projects/9-fetch-random-image'
 })
