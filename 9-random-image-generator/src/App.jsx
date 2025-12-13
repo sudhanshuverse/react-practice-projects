da626afc837a0e4ba7651082fe5779e31f67ff79
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-13 20:01:42 +0530

    files updated

--- 9-random-image-generator/src/App.jsx
+++ 9-random-image-generator/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const reload = () => {
+    window.location.reload()
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="main-container">
+      <header>
+        <nav>
+          <div className="left">
+            <h1>Xplore</h1>
+          </div>
+          <div className="right">
+            <a href="">Travel Guide</a>
+            <a href="">Famous Places</a>
+            <a href="">Contact Us</a>
+            <button>Booking</button>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div className="hero-section">
+            <button onClick={reload}>Change Image</button>
+        </div>
+      </main>
+    </div>
   )
 }
 
-export default App
+export default App;
